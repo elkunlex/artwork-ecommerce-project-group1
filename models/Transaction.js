8d5f80439ead374206b817b2b40a3865a4afff70
Andreapeach6 <andreapeach6@gmail.com>
2025-03-08 01:27:17 +0100

    added models codes for artwork, cart, trans, user

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,1 +1,13 @@
 //TRANSACTIONMODEL
+// models/Transaction.js
+const mongoose = require("mongoose");
+
+const TransactionSchema = new mongoose.Schema({
+  buyer: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  seller: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  artwork: { type: mongoose.Schema.Types.ObjectId, ref: "Artwork" },
+  dateOfPurchase: { type: Date, default: Date.now },
+  totalPrice: { type: Number, required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Transaction", TransactionSchema);
