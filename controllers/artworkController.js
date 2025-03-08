48b25ea27d73a7cd1bcad2b0c69bc87d552489af
Bolanle <bolanleayokunle007@gmail.com>
2025-03-08 01:14:16 +0100

    i added my controller

--- controllers/artworkController.js
+++ controllers/artworkController.js
@@ -1,1 +1,76 @@
-//artwork   
+// controllers/artworkController.js
+const Artwork = require("../models/Artwork");
+
+const createArtwork = async (req, res) => {
+  const { title, description, price, imageUrl, category } = req.body;
+
+  try {
+    const artwork = await Artwork.create({
+      title,
+      description,
+      price,
+      imageUrl,
+      category,
+      seller: req.user.id,
+    });
+
+    res.status(201).json(artwork);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getAllArtworks = async (req, res) => {
+  try {
+    const artworks = await Artwork.find({ status: "available" });
+    res.json(artworks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const updateArtwork = async (req, res) => {
+  const { title, description, price, imageUrl, category } = req.body;
+
+  try {
+    let artwork = await Artwork.findById(req.params.id);
+    if (!artwork) {
+      return res.status(404).json({ message: "Artwork not found" });
+    }
+
+    if (artwork.seller.toString() !== req.user.id) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+
+    artwork = await Artwork.findByIdAndUpdate(
+      req.params.id,
+      { title, description, price, imageUrl, category },
+      { new: true }
+    );
+
+    res.json(artwork);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteArtwork = async (req, res) => {
+  try {
+    const artwork = await Artwork.findById(req.params.id);
+
+    if (!artwork) {
+      return res.status(404).json({ message: "Artwork not found" });
+    }
+
+    if (artwork.seller.toString() !== req.user.id) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+
+    await artwork.remove();
+    res.json({ message: "Artwork deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { createArtwork, getAllArtworks, updateArtwork, deleteArtwork };
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,77 @@
-//user
+// controllers/userController.js
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user.id,
+        username: user.username,
+        email: user.email,
+        token: generateToken(user.id),
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        _id: user.id,
+        username: user.username,
+        email: user.email,
+        token: generateToken(user.id),
+      });
+    } else {
+      res.status(401).json({ message: "Invalid email or password" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
+
+module.exports = { registerUser, loginUser, getUserProfile };
