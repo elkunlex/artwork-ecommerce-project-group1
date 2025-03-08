0090b2bc1838fbd7523f919aef7e93e14c213ebf
Funmilayo <funmilayoibj@gmail.com>
2025-03-08 01:35:19 +0100

    implement the routes for users, artworks, cart, and transactions.

--- routes/artworkRoutes.js
+++ routes/artworkRoutes.js
@@ -1,1 +1,13 @@
-//artworkRoutes
+// routes/artworkRoutes.js
+const express = require("express");
+const { createArtwork, getAllArtworks, updateArtwork, deleteArtwork } = require("../controllers/artworkController");
+const { protect } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.post("/", protect, createArtwork);
+router.get("/", getAllArtworks);
+router.put("/:id", protect, updateArtwork);
+router.delete("/:id", protect, deleteArtwork);
+
+module.exports = router;
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,1 +1,12 @@
-//CARTROUTES
+// routes/cartRoutes.js
+const express = require("express");
+const { addToCart, removeFromCart, getCart } = require("../controllers/cartController");
+const { protect } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.post("/add", protect, addToCart);
+router.post("/remove", protect, removeFromCart);
+router.get("/", protect, getCart);
+
+module.exports = router;
